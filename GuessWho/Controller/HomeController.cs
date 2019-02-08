7d1afc33b5d5a36aacd70a01c80db6e19f63937e
Bryan Foster <bpfoster85@gmail.com>
2019-02-08 16:24:33 -0700

    Add files via upload

---
+++ GuessWho/Controller/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using GuessWho.Pages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuessWho.Controller
+{
+    public class HomeController: Microsoft.AspNetCore.Mvc.Controller
+    {
+        public IActionResult Index(IndexModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PictureUpload(IFormFile file)
+        {
+
+            IndexModel model = new IndexModel();
+            model.FileUploaded = true;
+            return RedirectToAction("Index", model);
+        }        
+    }
+}
---
+++ GuessWho/Pages/Shared/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GuessWho.Pages
+{
+    public class IndexModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public bool FileUploaded { get; set; }
+        public IndexModel()
+        {
+            FileUploaded = false;
+        }
+    }
+}
